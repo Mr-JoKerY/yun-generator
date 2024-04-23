16da426731bc890d01a9e1ab24601e0f839037c3
hyw <hyw@example.com>
2024-04-24 01:35:40 +0800

    增加元信息校验，抽取生成器模板

--- yun-generator-marker/src/main/java/com/hyw/marker/Main.java
+++ yun-generator-marker/src/main/java/com/hyw/marker/Main.java
@@ -1,15 +1,15 @@
 package com.hyw.marker;
 
-import com.hyw.marker.cli.CommandExecutor;
+import com.hyw.marker.generator.main.MainGenerator;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-//        args = new String[]{"generate", "-l", "-a", "-o"};
-//        args = new String[]{"config"};
-//        args = new String[]{"list"};
-        CommandExecutor commandExecutor = new CommandExecutor();
-        commandExecutor.doExecute(args);
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator mainGenerator = new MainGenerator();
+        mainGenerator.doGenerate();
     }
 
 }
