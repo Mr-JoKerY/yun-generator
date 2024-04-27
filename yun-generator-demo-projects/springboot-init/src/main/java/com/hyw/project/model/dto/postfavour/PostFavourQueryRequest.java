ce3b527d75f044dfc35e91ad3869cba4875cb810
hyw <hyw@example.com>
2024-04-27 23:41:22 +0800

    添加测试项目代码

---
+++ yun-generator-demo-projects/springboot-init/src/main/java/com/hyw/project/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,27 @@
+package com.hyw.project.model.dto.postfavour;
+
+import com.hyw.project.common.PageRequest;
+import com.hyw.project.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
