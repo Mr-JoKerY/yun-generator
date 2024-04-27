b31488bfa36fbff81f96e2edc3fe021922567a3a
hyw <hyw@example.com>
2024-04-27 23:40:57 +0800

    添加分组功能

---
+++ yun-generator-marker/src/test/java/com/hyw/marker/template/TemplateMakerTest.java
@@ -0,0 +1,47 @@
+package com.hyw.marker.template;
+
+import com.hyw.marker.meta.Meta;
+import com.hyw.marker.template.model.TemplateMakerFileConfig;
+import com.hyw.marker.template.model.TemplateMakerModelConfig;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class TemplateMakerTest {
+
+    /**
+     * 测试同配置多次生成时，强制变为静态生成
+     */
+    @Test
+    public void testMakeTemplateBug1() {
+        Meta meta = new Meta();
+        meta.setName("acm-template-generator");
+        meta.setDescription("ACM 示例模板生成器");
+
+        String projectPath = System.getProperty("user.dir");
+        String originProjectPath = new File(projectPath).getParent() + File.separator + "yun-generator-demo-projects/springboot-init";
+
+        // 文件参数配置
+        String inputFilePath1 = "src/main/resources/application.yml";
+        TemplateMakerFileConfig templateMakerFileConfig = new TemplateMakerFileConfig();
+        TemplateMakerFileConfig.FileInfoConfig fileInfoConfig1 = new TemplateMakerFileConfig.FileInfoConfig();
+        fileInfoConfig1.setPath(inputFilePath1);
+        templateMakerFileConfig.setFiles(Arrays.asList(fileInfoConfig1));
+
+        // 模型参数配置
+        TemplateMakerModelConfig templateMakerModelConfig = new TemplateMakerModelConfig();
+        TemplateMakerModelConfig.ModelInfoConfig modelInfoConfig1 = new TemplateMakerModelConfig.ModelInfoConfig();
+        modelInfoConfig1.setFieldName("url");
+        modelInfoConfig1.setType("String");
+        modelInfoConfig1.setDefaultValue("jdbc:mysql://localhost:3306/my_db");
+        modelInfoConfig1.setReplaceText("jdbc:mysql://localhost:3306/my_db");
+        List<TemplateMakerModelConfig.ModelInfoConfig> modelInfoConfigList = Arrays.asList(modelInfoConfig1);
+        templateMakerModelConfig.setModels(modelInfoConfigList);
+
+        long id = TemplateMaker.makeTemplate(meta, originProjectPath, templateMakerFileConfig, templateMakerModelConfig, 1784223512610250752L);
+        System.out.println(id);
+    }
+
+}
